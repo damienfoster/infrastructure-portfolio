494d0ae54c2deedcbb76aeb96fd8157e5af48c4d
damienfoster <Damien_foster19@yahoo.com>
2026-02-20 11:59:20 -0500

    Update hero.js

--- src/components/sections/hero.js
+++ src/components/sections/hero.js
@@ -60,14 +60,14 @@ const Hero = () => {
   }, []);
 
   const one = <h1>Hi, my name is</h1>;
-  const two = <h2 className="big-heading">Brittany Chiang.</h2>;
-  const three = <h3 className="big-heading">I build things for the web.</h3>;
+  const two = <h2 className="big-heading">Damien Foster.</h2>;
+  const three = <h3 className="big-heading">I build and secure network infrastructure.</h3>;
   const four = (
     <>
       <p>
-        I’m a software engineer specializing in building (and occasionally designing) exceptional
-        digital experiences. Currently, I’m focused on building accessible, human-centered products
-        at{' '}
+        I’m a network engineer specializing in designing, deploying and securing mission-critical
+        infrastructure. Currently, I’m focused on optimizing network performance and implementing
+        robust security protocols for enterprise environments.
         <a href="https://upstatement.com/" target="_blank" rel="noreferrer">
           Upstatement
         </a>
