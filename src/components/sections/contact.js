f344e03fb3e9168c79b3d2822ca0d6fd4a786f77
damienfoster <Damien_foster19@yahoo.com>
2026-02-20 14:53:26 -0500

    Update contact.js

--- src/components/sections/contact.js
+++ src/components/sections/contact.js
@@ -60,8 +60,8 @@ const Contact = () => {
       <h2 className="title">Get In Touch</h2>
 
       <p>
-        Although I’m not currently looking for any new opportunities, my inbox is always open.
-        Whether you have a question or just want to say hi, I’ll try my best to get back to you!
+        I’m always happy to connect with fellow engineers to discuss network architecture, home lab optimizations, or emerging security trends.
+        Whether you have a specific question or just want to talk shop, my inbox is always open!
       </p>
 
       <a className="email-link" href={`mailto:${email}`}>
