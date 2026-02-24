7c5380c79f3288aecbb68e373b3afc0e55bb2bfb
damienfoster <Damien_foster19@yahoo.com>
2026-02-24 12:21:47 -0500

    Update footer.js

--- src/components/footer.js
+++ src/components/footer.js
@@ -108,18 +108,7 @@ const Footer = () => {
         <a href="https://github.com/bchiang7/v4">
           <div>Designed &amp; Built by Damien Foster. Based on a design by Brittany Chiang</div>
 
-          {githubInfo.stars && githubInfo.forks && (
-            <div className="github-stats">
-              <span>
-                <Icon name="Star" />
-                <span>{githubInfo.stars.toLocaleString()}</span>
-              </span>
-              <span>
-                <Icon name="Fork" />
-                <span>{githubInfo.forks.toLocaleString()}</span>
-              </span>
-            </div>
-          )}
+
         </a>
       </StyledCredit>
     </StyledFooter>
