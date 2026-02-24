b828cf1bc8c36f3ffee697c81574ba31abc8a1be
damienfoster <Damien_foster19@yahoo.com>
2026-02-24 12:59:20 -0500

    Update nav.js

--- src/components/nav.js
+++ src/components/nav.js
@@ -143,11 +143,6 @@ const StyledLinks = styled.div`
     }
   }
 
-  .resume-button {
-    ${({ theme }) => theme.mixins.smallButton};
-    margin-left: 15px;
-    font-size: var(--fz-xs);
-  }
 `;
 
 const Nav = ({ isHome }) => {
@@ -205,12 +200,6 @@ const Nav = ({ isHome }) => {
     </div>
   );
 
-  const ResumeLink = (
-    <a className="resume-button" href="/resume.pdf" target="_blank" rel="noopener noreferrer">
-      Resume
-    </a>
-  );
-
   return (
     <StyledHeader scrollDirection={scrollDirection} scrolledToTop={scrolledToTop}>
       <StyledNav>
@@ -227,7 +216,7 @@ const Nav = ({ isHome }) => {
                     </li>
                   ))}
               </ol>
-              <div>{ResumeLink}</div>
+              
             </StyledLinks>
 
             <Menu />
@@ -257,15 +246,7 @@ const Nav = ({ isHome }) => {
                 </TransitionGroup>
               </ol>
 
-              <TransitionGroup component={null}>
-                {isMounted && (
-                  <CSSTransition classNames={fadeDownClass} timeout={timeout}>
-                    <div style={{ transitionDelay: `${isHome ? navLinks.length * 100 : 0}ms` }}>
-                      {ResumeLink}
-                    </div>
-                  </CSSTransition>
-                )}
-              </TransitionGroup>
+
             </StyledLinks>
 
             <TransitionGroup component={null}>
