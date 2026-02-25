41473a6074a328ee3229f51e6dfae7d925d1d527
damienfoster <Damien_foster19@yahoo.com>
2026-02-25 11:47:49 -0500

    Update featured.js

--- src/components/sections/featured.js
+++ src/components/sections/featured.js
@@ -319,7 +319,6 @@ const Featured = () => {
               tech
               github
               external
-              cta
             }
             html
           }
@@ -379,11 +378,7 @@ const Featured = () => {
                     )}
 
                     <div className="project-links">
-                      {cta && (
-                        <a href={cta} aria-label="Course Link" className="cta">
-                          Learn More
-                        </a>
-                      )}
+
                       {github && (
                         <a href={github} aria-label="GitHub Link">
                           <Icon name="GitHub" />
